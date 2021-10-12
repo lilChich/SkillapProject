b10a8db7e7d6aafca89e691d316a0a51b5ec896b
WhoDatBoi <bohdan.butenko@nure.ua>
2021-10-12 14:34:34 +0300

    Add project files.

---
+++ Skillap.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Skillap.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ Skillar.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Skillar.DAL
+{
+    public class Class1
+    {
+    }
+}
