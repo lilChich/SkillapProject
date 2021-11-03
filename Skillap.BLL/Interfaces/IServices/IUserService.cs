4a157d382c6f78c04b2bb5bb75499abb2aed4241
WhoDatBoi <bohdan.butenko@nure.ua>
2021-11-03 18:13:09 +0200

    REST exeption + Interfaces for UserService and JWT was added.

---
+++ Skillap.BLL/Interfaces/IServices/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Skillap.BLL.DTO;
+
+namespace Skillap.BLL.Interfaces.IServices
+{
+    public interface IUserService
+    {
+       
+    }
+}
