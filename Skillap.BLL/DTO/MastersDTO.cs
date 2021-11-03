2e349e207f6c37738cb33280fa64377fa3f52362
WhoDatBoi <bohdan.butenko@nure.ua>
2021-11-03 18:12:02 +0200

    Data Table Objects was added to project.

---
+++ Skillap.BLL/DTO/MastersDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skillap.BLL.DTO
+{
+    public class MastersDTO
+    {
+        public int Id { get; set; }
+        public bool Status { get; set; }
+        public int SkillLevel { get; set; }
+
+        public int MasterClassId { get; set; }
+        public int ApplicationUserId { get; set; }
+    }
+}
---
+++ Skillap.BLL/DTO/PostsDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skillap.BLL.DTO
+{
+    public class PostsDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public bool Status { get; set; }
+    }
+}
