e86894c026eac22022c25d17fe9698638930dd4a
WhoDatBoi <bohdan.butenko@nure.ua>
2021-11-24 00:26:28 +0200

    IMasterService was added without realization

---
+++ Skillap.BLL/Interfaces/IServices/IMasterService.cs
@@ -0,0 +1,21 @@
+﻿using Skillap.BLL.DTO;
+using Skillap.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skillap.BLL.Interfaces.IServices
+{
+    public interface IMasterService
+    {
+        public Task CreateMasterClass(MasterClassesDTO masterClassesDto);
+        public Task<bool> UpdateMasterClass(MasterClassesDTO masterClassesDto);
+        public Task<bool> DeleteMasterClass(MasterClassesDTO masterClassesDto);
+        public Task<MasterClassesDTO> GetMasterClassById(int id);
+
+        public Task<bool> StatusInMastersTable(MastersDTO mastersDto);
+        public Task<int> ChangeSkillLevel(MastersDTO mastersDto);
+    }
+}
